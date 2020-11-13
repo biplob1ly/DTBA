53dd1d98627de0361edf00e87eb6bf903cc18036
JSmith16 <jaykus2@gmail.com>
2020-11-13 11:48:31 -0500

    Added basic transformer model

--- models.py
+++ models.py
@@ -1,7 +1,8 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-
+import math
+from torch.nn import TransformerEncoder, TransformerEncoderLayer
 
 class CNNModel(nn.Module):
     def __init__(self, args):
@@ -37,6 +38,76 @@ class CNNModel(nn.Module):
         out = self.output(DTI)
         return out
 
+class PositionalEncoding(nn.Module):
+    def __init__(self, d_model, dropout=.1, max_len=5000):
+        """
+        Taken from pytorch tutorial on Transformers
+        https://pytorch.org/tutorials/beginner/transformer_tutorial.html
+        Which in turn seems to be taken from Zelun Wang
+        https://github.com/wzlxjtu/PositionalEncoding2D/blob/master/positionalembedding2d.py
+        
+        d_model: size of input vectors
+        max_len: length of positions
+        
+        returns max_length * d_model position matrix
+        """
+        super(PositionalEncoding, self).__init__()
+        self.dropout = nn.Dropout(dropout)
+        
+        encoding = torch.zeros(max_len, d_model)
+        position = torch.arage(0, max_len, dtype=torch.float).unsqueeze(1)
+        div_term = torch.exp(torch.arange(0, d_model, 2).float() * (-math.log(10000.0) / d_model))
+        encoding[:, 0::2] = torch.sin(position * div_term)
+        encoding[:, 1::2] = torch.cos(position * div_term)
+        encoding = encoding.unsqueeze(0).transpose(0,1)
+        self.register_buffer('encoding', encoding)
+    
+    def forward(self, x):
+        x = x + self.encoding[:x.size(0), :]
+        return self.dropout(x)
 
+class Transformer(nn.Module):
+    def __init__(self, num_embeddings, input_size, n_head, hidden_size, n_layers, dropout=.1):
+        """
+        Taken from pytorch tutorial on Transformers
+        https://pytorch.org/tutorials/beginner/transformer_tutorial.html
+        
+        num_embeddings: Size of input dictionary
+        input_size: Maximum size of inputs
+        n_head: number of heads in multihead attention
+        hidden_size: size of embeddings/hidden layers
+        n_layers: number of encoding layers in transformer
+        """
+        super(Transformer, self).__init__()
+        self.pos_encoder = PositionalEncoding(input_size, dropout)
+        encoder_layers = TransformerEncoderLayer(input_size, n_head, hidden_size, dropout)
+        self.transformer_encoder = TransformerEncoder(encoder_layers, n_layers)
+        self.encoder = nn.Embedding(num_embeddings, input_size)
+        self.input_size = input_size
+        self.decoder = nn.Linear(input_size, num_embeddings)
+        
+        self.init_weights()
+    
+    def generate_square_subsequent_mask(self, size):
+        """
+        I'm actually not sure what purpose this function has
+        But I copied it down anyway - maybe it solves a problem I don't understand yet.
+        """
+        mask = (torch.triu(torch.ones(size, size)) == 1).transpose(0,1)
+        mask = mask.float().masked_fill(mask == 0, float('-inf')).masked_fill(mask == 1, float(0.0))
+        return mask
+        
+    def init_weights(self):
+        init_range = 0.1
+        self.encoder.weight.data.uniform_(-init_range, init_range)
+        self.decoder.bias.data.zero_()
+        self.decoder.weight.data.uniform_(-init_range, init_range)
+        
+    def forward(self, x, x_mask):
+        x = self.encoder(x) * math.sqrt(self.input_size)
+        x = self.pos_encoder(x)
+        output = self.transformer_encoder(x, x_mask)
+        output = self.decoder(x)
+        return output
 
 
